5629f728a21c380dbc16e4b655b7427ba73685f8
Pooja Nair <Pooja Nair>
2016-08-17 22:30:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+//package com.example.listview_static2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity  {
+
+    //private static final String EXTRA_MESSAGE = "TEST";
+    public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        String[] Services = {" DIAG", " TMS", " TIME"," UTILS"," QDSS"};
+        if(Integer.parseInt(message)<Services.length)
+            message = message+Services[Integer.parseInt(message)-1];
+        else
+            message = "Invalid Input";
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
+
+
+}
