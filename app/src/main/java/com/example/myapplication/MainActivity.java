e7def9bb2f67aa254893301bf09735f99de8b2f6
Devang Verma <devangverma@gmail.com>
2016-08-18 23:13:54 -0700

    Added logic to enter numeric and alphabetical values to text box and check if it exist. Added check for values lower than 0, greater than the size of the list, etc.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.example.myapplication.MESSAGE";
-    public static final List<String> Services = Arrays.asList("DIAG", "TMS", "TIME", "UTILS", "QDSS");
+    public static final List<String> Services = Arrays.asList("diag", "tms", "time", "utils", "qdss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +26,14 @@ public class MainActivity extends AppCompatActivity {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
 
-        if (message.matches("[a-zA-Z]+") && Services.contains(message)) {
-            Log.i(message, "message ");
-            message = Integer.toString(Integer.parseInt(String.valueOf(Services.indexOf(message)))+1) + message;
-        } else if (message.matches("[0-9]+") && (Integer.parseInt(message) < Services.size())) {
-            Log.i(message, "message ");
-            message = message + Services.get(Integer.parseInt(message) - 1);
+        if (message.matches("[a-zA-Z]+") && Services.contains(message.toLowerCase())) {
+            //Log.i(message, "message ");
+            message = Integer.toString(1+Integer.parseInt(String.valueOf(Services.indexOf(message.toLowerCase())))) +" "+message.toUpperCase();
+        } else if (message.matches("[0-9]+") && (Integer.parseInt(message) <= Services.size()) && (Integer.parseInt(message) >0)) {
+            //Log.i(message, "message ");
+            message = message +" "+Services.get(Integer.parseInt(message) - 1).toUpperCase();
         } else
-            message = "Invalid Input";
+            message = "Invalid Input: Please enter 1-5 or test name( DIAG, TMS, TIME, UTILS, QDSS)";
 
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
